a1b05c6a27b6a1e885095fad02fa05bfa74415cf
Thad Kerosky <contact@thadk.net>
2017-06-29 01:17:51 +0000

    remove 2x

--- index.js
+++ index.js
@@ -21,15 +21,13 @@ app.use('/', httpProxy({
     toProxy:  true,
     autoRewrite:  true,
     router: function (req) {
-        if (req.path.indexOf('/v3/stamen.i808gmk6/') >-1 ) {
-          var myRegexp = /(stamen.i808gmk6\/)(\w+)\/(\w+)\/(\w+)/gi;
+        if (req.path.indexOf('/v3/stamen.i808gmk5/') >-1 ) {
+          var myRegexp = /(stamen.i808gmk6\/)(\w+)\/(\w+)\/(\w+)/g
           var match = myRegexp.exec(req.path);
-          console.log(match)
-        return `${API_SERVER}/styles/v1/thadk/cj4fts6o83n4a2rnpnqi6uuk6/tiles/256/${match[2]}/${match[3]}/${match[4]}?access_token=pk.eyJ1IjoidGhhZGsiLCJhIjoidWlIX1J4OCJ9.BZnfrT_Jdc8aSQyWV8fL7g`
+
+        return `${API_SERVER}/styles/v1/thadk/cj4fts6o83n4a2rnpnqi6uuk6/tiles/256/17/61618/63242@2x?access_token=pk.eyJ1IjoidGhhZGsiLCJhIjoidWlIX1J4OCJ9.BZnfrT_Jdc8aSQyWV8fL7g`
       }
     },
-    pathRewrite: {'^v3/stamen.i808gmk5/17/61617/63244.png' : '/new/api'}
-
 
 }));
 
