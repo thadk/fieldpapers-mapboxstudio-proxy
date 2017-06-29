2cd59d50968504f68e290e1d46ff4fd143e51d2a
Thad Kerosky <contact@thadk.net>
2017-06-29 01:31:18 +0000

    remove 2x

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ app.use('/', httpProxy({
     toProxy:  true,
     autoRewrite:  true,
     router: function (req) {
-        if (req.path.indexOf('/v3/stamen.i808gmk3/') >-1 ) {
-          var myRegexp = /(stamen.i808gmk3\/)(\w+)\/(\w+)\/(\w+)/gi;
+        if (req.path.indexOf('/v3/stamen.i808gmk0/') >-1 ) {
+          var myRegexp = /(stamen.i808gmk0\/)(\w+)\/(\w+)\/(\w+)/gi;
           var match = myRegexp.exec(req.path);
           console.log(match)
         return `${API_SERVER}/styles/v1/thadk/cj4fts6o83n4a2rnpnqi6uuk6/tiles/256/${match[2]}/${match[3]}/${match[4]}?access_token=pk.eyJ1IjoidGhhZGsiLCJhIjoidWlIX1J4OCJ9.BZnfrT_Jdc8aSQyWV8fL7g`
